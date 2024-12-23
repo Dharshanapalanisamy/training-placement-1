8d05bf836a5061ffe645e7c640495bd2486ec402
Darshana P.S <151154925+Dharshanapalanisamy@users.noreply.github.com>
2024-12-23 18:46:54 +0530

    Create Qn-9.java

---
+++ 24.12.2024/Qn-9.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26]; 
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isAnagram("anagram", "nagaram")); 
+        System.out.println(solution.isAnagram("rat", "car"));        
+    }
+}
